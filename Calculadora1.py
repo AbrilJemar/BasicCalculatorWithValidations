4135fe1e5f21dab0ac901f704be40ccb3cea8256
Abril Jemar <121254254+AbrilJemar@users.noreply.github.com>
2023-01-24 11:49:51 -0300

    Update Calculadora1.py

--- Calculadora1.py
+++ Calculadora1.py
@@ -1,3 +1,17 @@
+def check_user_input(x):
+    try:
+        # Convert it into integer
+        val = int(x)
+    except ValueError:
+        try:
+            # Convert it into float
+            val = float(x)
+        except ValueError:
+            val = x
+
+    return (val)
+
+
 while True:
     print("""
     Tell me, what do you want to do?
@@ -9,78 +23,79 @@ while True:
     5) Turn off the calculator
 
     """)
-    option = input("Choose an option: ")    
+    option = input("    Choose an option: ")    
     option = check_user_input(option)
     while(isinstance(option, str) or option not in [1,2,3,4,5]):
-        option = input("Invalid option. Try again: ")
+        option = input("    Invalid option. Try again: ")
         option = check_user_input(option)
         
     match option:
         case 1:
-            number_one = input("Enter first number: ")
+            number_one = input("    Enter first number: ")
             number_one = check_user_input(number_one)
             while(isinstance(number_one, str)):
-                number_one = input("Invalid number. Try again: ")
+                number_one = input("    Invalid number. Try again: ")
                 number_one = check_user_input(number_one)
     
-            number_two = input("Enter second number: ")
-            number_two = check_user_input(number_two)
+                number_two = input("    Enter second number: ")
+                number_two = check_user_input(number_two)
             while(isinstance(number_two, str)):
-                number_two = input("Invalid number. Try again: ")
+                number_two = input("    Invalid number. Try again: ")
                 number_two = check_user_input(number_two)
 
             Operation_Sum = number_one + number_two
-            print('The sum is: ' + str(Operation_Sum))
+            print('\r\n     The sum is: ' + str(Operation_Sum))
         
         case 2:
-            number_one = input("Enter first number: ")
+            number_one = input("    Enter first number: ")
             number_one = check_user_input(number_one)
             while(isinstance(number_one, str)):
-                number_one = input("Invalid number. Try again: ")
+                number_one = input("    Invalid number. Try again: ")
                 number_one = check_user_input(number_one)
     
-            number_two = input("Enter second number: ")
-            number_two = check_user_input(number_two)
+                number_two = input("    Enter second number: ")
+                number_two = check_user_input(number_two)
             while(isinstance(number_two, str) ):
-                number_two = input("Invalid number. Try again: ")
+                number_two = input("    Invalid number. Try again: ")
                 number_two = check_user_input(number_two)
 
             Operation_Subtra = number_one - number_two
-            print('The subtraction is: ' + str(Operation_Subtra))
+            print('\r\n    The subtraction is: ' + str(Operation_Subtra))
 
         case 3:
-            number_one = input("Enter first number: ")
+            number_one = input("    Enter first number: ")
             number_one = check_user_input(number_one)
             while(isinstance(number_one, str)):
-                number_one = input("Invalid number. Try again: ")
+                number_one = input("    Invalid number. Try again: ")
                 number_one = check_user_input(number_one)
     
-            number_two = input("Enter second number: ")
-            number_two = check_user_input(number_two)
+                number_two = input("    Enter second number: ")
+                number_two = check_user_input(number_two)
             while(isinstance(number_two, str)):
-                number_two = input("Invalid number. Try again: ")
+                number_two = input("    Invalid number. Try again: ")
                 number_two = check_user_input(number_two)
 
             Operation_Multi = number_one * number_two
-            print('The product is: ' + str(Operation_Multi))
+            print('\r\n    The product is: ' + str(Operation_Multi))
 
         case 4:
-            number_one = input("Enter first number: ")
+            number_one = input("    Enter first number: ")
             number_one = check_user_input(number_one)
             while(isinstance(number_one, str)):
-                number_one = input("Invalid number. Try again: ")
+                number_one = input("    Invalid number. Try again: ")
                 number_one = check_user_input(number_one)
     
-            number_two = input("Enter second number: ")
-            number_two = check_user_input(number_two)
+                number_two = input("    Enter second number: ")
+                number_two = check_user_input(number_two)
             while(isinstance(number_two, str) or number_two == 0):
-                number_two = input("Invalid number. Try again: ")
+                number_two = input("    Invalid number. Try again: ")
                 number_two = check_user_input(number_two)
 
             Operation_Divide = number_one / number_two
-            print('The division is: ' + str(Operation_Divide))
+            print('\r\n    The division is: ' + str(Operation_Divide))
 
         case 5:
             break
 
-print("OFF")
+
+print('\r\n    OFF')
